f21c1bc7c257f387997e96218db518c77d3278de
Emran <ehossin3@gmail.com>
2024-12-17 22:08:57 +0600

    Create a main menu with register and display functionality, apply custom CSS to the ul, li, and a tags, dynamically add an active class to menu items, and ensure the 'Home' page always has the active class.

--- functions.php
+++ functions.php
@@ -1,27 +1,75 @@
 <?php
 
-    if(!function_exists('neogym_bootstrapping')){
-        function neogym_bootstrapping()
-        {
-            load_theme_textdomain('neogym');
-            add_theme_support('post-thumbnails',['post']);
-            add_theme_support('post-formats', ['aside', 'gallery', 'quote', 'image', 'video']);
+if (!function_exists('neogym_bootstrapping')) {
+    function neogym_bootstrapping()
+    {
+        load_theme_textdomain('neogym');
+        add_theme_support('post-thumbnails', ['post']);
+        add_theme_support('post-formats', ['aside', 'gallery', 'quote', 'image', 'video']);
+        register_nav_menus(
+            [
+                'main_menu' => __('Main Menu', 'neogym'),
+            ]
+        );
+    }
+}
+
+add_action('after_setup_theme', 'neogym_bootstrapping');
+
+//excerpt custom length
+// if(!function_exists('neogym_custom_excerpt_length')){
+//     function neogym_custom_excerpt_length($length)
+//     {
+//         return 5;
+//     }
+// }
+
+// add_filter('excerpt_length', 'neogym_custom_excerpt_length');
+function get_custom_excerpt($limit)
+{
+    $excerpt = wp_trim_words(get_the_excerpt(), $limit);
+    return $excerpt;
+}
+
+/* This code snippet is defining a function `neogym_add_custom_class_to_main_menu` that is used as a
+filter callback to add custom classes to the main menu items in WordPress. Here's a breakdown of
+what the function does: */
+
+if (!function_exists('neogym_add_custom_class_to_main_menu')) {
+    function neogym_add_custom_class_to_main_menu($classes, $item, $args)
+    {
+        if ($args->theme_location === 'main_menu') {
+            $classes[] = 'nav-item';
+
+            if (in_array('current-menu-item', $item->classes) || in_array('current-page-ancestor', $item->classes)) {
+                if (!is_front_page() && $item->url === home_url('/')) {
+                    return $classes;
+                }
+
+                $classes[] = 'active';
+            }
+
         }
-    }
-    
-    add_action('after_setup_theme', 'neogym_bootstrapping');
 
-    //excerpt custom length
-    // if(!function_exists('neogym_custom_excerpt_length')){
-    //     function neogym_custom_excerpt_length($length)
-    //     {
-    //         return 5;
-    //     }
-    // }
-
-    // add_filter('excerpt_length', 'neogym_custom_excerpt_length');
-    function get_custom_excerpt($limit) {
-        $excerpt = wp_trim_words(get_the_excerpt(), $limit);
-        return $excerpt;
+        return $classes;
     }
-    
+}
+
+add_filter('nav_menu_css_class', 'neogym_add_custom_class_to_main_menu', 10, 3);
+
+
+/* This code snippet defines a function `neogym_add_custom_class_to_main_menu_links` that is used as a
+filter callback to add a custom class to the attributes of main menu links in WordPress. */
+
+if (!function_exists('neogym_add_custom_class_to_main_menu_links')) {
+    function neogym_add_custom_class_to_main_menu_links($attrs, $item, $args)
+    {
+        if ($args->theme_location === 'main_menu') {
+            $attrs['class'] = 'nav-link';
+
+        }
+
+        return $attrs;
+    }
+}
+add_filter('nav_menu_link_attributes', 'neogym_add_custom_class_to_main_menu_links', 10, 3);
--- header.php
+++ header.php
@@ -45,21 +45,13 @@
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
-              <ul class="navbar-nav  ">
-                <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item ">
-                  <a class="nav-link" href="why.html"> Why us </a>
-                </li>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="trainer.html"> trainers</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.html"> Contact Us</a>
-                </li>
-              </ul>
+              <?php 
+                $args = [
+                  'theme_location'  => 'main_menu',
+                  'menu_class'        => 'navbar-nav',
+                ];
+                wp_nav_menu($args);
+              ?>
               <div class="user_option">
                 <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
                   <button style="background-image: url('<?php echo get_template_directory_uri() . '/assets/' ?>images/search-icon.png);" class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
